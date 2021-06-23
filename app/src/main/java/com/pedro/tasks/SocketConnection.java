a5ecf2ab78b871928e117c7af69033a68a724766
Jarnotouw14 <Jarnotouw14@gmail.com>
2021-06-23 13:31:54 +0200

    added socket.io client

--- app/src/main/java/com/pedro/tasks/SocketConnection.java
+++ app/src/main/java/com/pedro/tasks/SocketConnection.java
@@ -1,5 +1,7 @@
 package com.pedro.tasks;
 
+import android.text.TextUtils;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -7,8 +9,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.Socket;
+import java.net.URISyntaxException;
 
+import io.socket.client.IO;
+import io.socket.client.Socket;
 
 public class SocketConnection {
     private Socket clientSocket;
@@ -17,32 +21,42 @@ public class SocketConnection {
 
 
     public void startConnection(String ip, int port) {
-        Socket clientSocket = null;
+        //Socket clientSocket = null;
         try {
-            clientSocket = new Socket("http://localhost/", 3000);
-            out = new PrintWriter(clientSocket.getOutputStream(), true);
-            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        } catch (IOException e) {
+            // ws://seechange-chat.the-circle.designone.nl:80
+            clientSocket = IO.socket("http://localhost:3000");
+            clientSocket.connect();
+            clientSocket.emit("joinstream", "");
+            //out = new PrintWriter(clientSocket.getOutputStream(), true);
+            //in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        } catch (URISyntaxException e) {
             e.printStackTrace();
         }
     }
 
-        public String sendMessage(String msg) throws IOException {
-            JSONObject object = new JSONObject();
-            try {
-                object.put("_id", "?");
-                object.put("name", "message username");
-                object.put("date", "message.time");
-                object.put("message", "message.message");
-                object.put("info", false);
-            } catch (JSONException e) {
-                e.printStackTrace();
+        public void sendMessage(String msg) throws IOException {
+            System.out.println(msg);
+
+            if (TextUtils.isEmpty(msg)) {
+                return;
             }
 
-            System.out.println(object);
-            out.println(msg);
-            String resp = in.readLine();
-            return resp;
+            clientSocket.emit("chatMessage", msg);
+//            JSONObject object = new JSONObject();
+//            try {
+//                object.put("_id", "?");
+//                object.put("name", "message username");
+//                object.put("date", "message.time");
+//                object.put("message", "message.message");
+//                object.put("info", false);
+//            } catch (JSONException e) {
+//                e.printStackTrace();
+//            }
+//
+//            System.out.println(object);
+//            out.println(msg);
+//            String resp = in.readLine();
+//            return resp;
         }
 
         public void stopConnection() throws IOException {

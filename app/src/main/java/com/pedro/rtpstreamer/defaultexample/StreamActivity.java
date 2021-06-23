dd2b7a0a7ee1a5153c6c8985e0235923a7e6c10f
Vincent <vmf.bas@student.avans.nl>
2021-06-23 14:42:26 +0200

    camera bug fixed, connection fixed

--- app/src/main/java/com/pedro/rtpstreamer/defaultexample/StreamActivity.java
+++ app/src/main/java/com/pedro/rtpstreamer/defaultexample/StreamActivity.java
@@ -25,7 +25,6 @@ import com.pedro.rtpstreamer.MainActivity;
 import com.pedro.rtpstreamer.R;
 import com.pedro.rtpstreamer.utils.PathUtils;
 import com.pedro.tasks.GetItemAsyncTask;
-import com.pedro.tasks.SocketConnection;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -48,7 +47,7 @@ public class StreamActivity extends AppCompatActivity
   private RecyclerView.LayoutManager mLayoutManager;
   private String currentDateAndTime = "";
   private File folder;
-  private SocketConnection socket = new SocketConnection();
+//  private SocketConnection socket = new SocketConnection();
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -74,6 +73,7 @@ public class StreamActivity extends AppCompatActivity
     mLayoutManager = new LinearLayoutManager(this);
     //connect it to a layout manager
     mRecyclerView.setLayoutManager(mLayoutManager);
+
 
     //Init features array
     ArrayList<Message> mMessages = getMessages();
@@ -220,7 +220,9 @@ public class StreamActivity extends AppCompatActivity
 
   @Override
   public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
-    rtmpCamera1.startPreview();
+    if (!rtmpCamera1.isOnPreview()){
+      rtmpCamera1.startPreview();
+    }
   }
 
   @Override
